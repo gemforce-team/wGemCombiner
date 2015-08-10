3add23d1d7bf6eb3be565a5c89b97e83615c6c8f
Andrea Stacchiotti <andrea_stacchiotti@hotmail.it>
2015-08-10 07:26:57 +0200

    Improved help form

--- WGemCombiner/HelpForm.cs
+++ WGemCombiner/HelpForm.cs
@@ -42,7 +42,7 @@ namespace WGemCombiner
         // This is the only function you need to make changes to when adding new help pages
         private void fillHelpMessages()
         {
-            addHelpMessage("Working with the combiner", "\nPaste gem combining equations or a parenthesis formula into the textbox, then click 'Get Instructions'.\n" +
+            addHelpMessage("Working with the combiner", "Paste gem combining equations or a parenthesis formula into the textbox, then click 'Get Instructions'.\n" +
                 "Set the 'delay' to at LEAST as many miliseconds as a frame on GC2 is taking. I recommend at least 45-50 for no lag " +
                 "(it will usually work with much lower, but going any lower than the frame time will not actually speed up the process, " +
                 "as the game will only do one step per frame).\n" +
@@ -59,7 +59,7 @@ namespace WGemCombiner
                 "If your cursor has stopped moving, the program has stopped.\n\n" +
                 "The displayed list of instructions should be pretty self-explanatory. 1A is bottom-right, 1C is bottom-left, 12C is top-left.");
 
-            addHelpMessage("Speccing", "\nGem Combiner now supports speccing.\n" +
+            addHelpMessage("Speccing", "Gem Combiner now supports speccing.\n" +
                 "To spec, place base gems of different color in 1A, 1B, etc, as many colors as needed.\n" +
                 "\n" +
                 "Order of colors should be, starting at 1A: Orange, Yellow, Black, Red (then killgem and managem).\n" +
@@ -76,7 +76,7 @@ namespace WGemCombiner
                 "Press '" + parentForm1.hotkeyText + "' with your cursor over the Orange gem in 1A\n" +
                 "Enjoy!");
 
-            addHelpMessage("Input Format", "\nExample of combining equations:\n" +
+            addHelpMessage("Input Format", "Example of combining equations:\n" +
                 "(val = 1)\t0 = g1 orange\n(val = 2)\t1 = 0 + 0\n(val = 3)\t2 = 1 + 0\n" +
                 "Works if you have first gem as '1' and second as '2', etc, as well.\n" +
                 "\n" +
@@ -84,7 +84,7 @@ namespace WGemCombiner
                 "(2+1)+1\nor\n(1+0)+0\nor\n(2m+m)+m\n" +
                 "If zeros are present they are treated as 1s and 1s as 2s.");
 
-            addHelpMessage("Presets", "\nPreset schemes are supported. To use one, first select a color + combine/spec from the top drop-down list.\n" +
+            addHelpMessage("Presets", "Preset schemes are supported. To use one, first select a color + combine/spec from the top drop-down list.\n" +
                 "After selecting a color + combine/spec, the second drop-down list will display the preset options. First number is number of base gems, " +
                 "second number is the growth rate. Ones marked with a '-' are 2^n combines [over 8], and are NEVER the best for growth rate per cost " +
                 "(included for convenience if you want easily comparable gem costs)." +
@@ -95,13 +95,13 @@ namespace WGemCombiner
                 "but will be very similar to the gem ones");
 
 #if DEBUG
-            addHelpMessage("Importing new presets","\n In debug mode you can import new presets from a .txt file with recipes.\n"+
+            addHelpMessage("Importing new presets", "In debug mode you can import new presets from a .txt file with recipes.\n"+
                 "Your txt must contain one recipe per line."+
                 "The recipes have to be valid parenthesis formulas (not equations). The parser is somewhat robust but i wouldn't test it too hard :)\n"+
                 "To start importing, choose \"Import...\" option from the Preset drop-down, then choose the .txt file with recipes");
 #endif
 
-            addHelpMessage("Credits", "\nGem Combiner was made by Suuper.\n" +
+            addHelpMessage("Credits", "Gem Combiner was made by Suuper.\n" +
                 "\n" +
                 "Contributors:\n" +
                 "- 12345ieee (some spec parsing, preset recipes)\n" +
@@ -110,8 +110,11 @@ namespace WGemCombiner
                 "\n" +
                 "Release version: "+ releaseVersion + "\n" +
                 "\n" +
-                "To report a problem, go to\n" +
-                "https://github.com/gemforce-team/wGemCombiner/issues");
+                "To report a problem, see\n" +
+                "https://github.com/gemforce-team/wGemCombiner#report-bugs" + "\n" +
+                "\n" +
+                "If you wish to contribute/donate to the project, see\n" +
+                "https://github.com/gemforce-team/wGemCombiner#credits" + "\n");
         }
         string releaseVersion = "1.0.8";
 
@@ -209,6 +212,11 @@ namespace WGemCombiner
         private void closeHelpButton_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void helpMessageTextBox_LinkClicked(object sender, LinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.LinkText);
         }
 
     }
