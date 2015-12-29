c11215ccfc8a49ea31f6430aa92d5c9a56da4189
Andrea Stacchiotti <crudepatate@gmail.com>
2015-12-29 01:51:47 +0100

    Added portable release script

---
+++ release.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# wGC release script
+# you need to copy wGC .exe in the repo root
+# the compilation command with mono is:
+# xbuild /p:win32icon=Resources/graphics/GCIcon.ico /p:Configuration=Release
+
+
+cflags="-O3 -lm -I ""include"" -std=gnu11"
+
+if [ ! -e "WGemCombiner.exe" ]; then
+	echo "No wGC executable found, aborting..."
+	exit 1
+fi
+
+version=$(git describe --tags | cut -f1-2 -d "-")
+foldername="WGemCombiner-${version}"
+
+mkdir -p "${foldername}"
+cp WGemCombiner.exe         ${foldername}/WGemCombiner.exe
+cp LICENSE.txt              ${foldername}/LICENSE.txt
+cp README.md                ${foldername}/README.txt
+cp WGemCombiner/recipes.txt ${foldername}/recipes.txt
+
+echo "Building archive"
+zip -r "${foldername}.zip" "${foldername}"
