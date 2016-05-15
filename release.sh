90162c0c98abd9ddab1466abf140421d3227e518
Andrea Stacchiotti <crudepatate@gmail.com>
2016-05-15 12:36:49 +0200

    release.sh: Updated release script

--- release.sh
+++ release.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
-# wGC release script
-# you need to copy wGC .exe in the repo root
-# the compilation command with mono is:
-# xbuild /p:win32icon=Resources/graphics/GCIcon.ico /p:Configuration=Release
+### wGC release script ###
 
+buildfolder="WGemCombiner/bin/x86/Release"
+xbuild /p:win32icon=Resources/graphics/GCIcon.ico /p:Configuration=Release
 
-if [ ! -e "WGemCombiner.exe" ]; then
-	echo "No wGC executable found, aborting..."
-	exit 1
-fi
+version=$(git describe --tags | rev | cut -d '-' -f2- | rev)
+releasefolder="WGemCombiner-${version}"
 
-version=$(git describe --tags | cut -f1-2 -d "-")
-foldername="WGemCombiner-${version}"
-
-mkdir -p "${foldername}"
-cp WGemCombiner.exe         ${foldername}/WGemCombiner.exe
-cp LICENSE.txt              ${foldername}/LICENSE.txt
-cp README.md                ${foldername}/README.txt
-cp WGemCombiner/recipes.txt ${foldername}/recipes.txt
+mkdir -p "${releasefolder}"
+cp ${buildfolder}/WGemCombiner.exe  ${releasefolder}/WGemCombiner.exe
+cp LICENSE.txt                      ${releasefolder}/LICENSE.txt
+cp README.md                        ${releasefolder}/README.txt
+cp WGemCombiner/recipes.txt         ${releasefolder}/recipes.txt
 
 echo "Building archive"
-zip -r "${foldername}.zip" "${foldername}"
+zip -rTm "${releasefolder}.zip" "${releasefolder}"
